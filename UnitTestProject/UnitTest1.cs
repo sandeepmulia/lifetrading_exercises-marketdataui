b54c528f93c016a57717d074da05b66734c1a9f0
sandeepmulia <sandeepmulia@users.noreply.github.com>
2019-01-07 03:27:39 +1100

    Updated tests based on feedback
    1. Used Moq
    2. Removed Thread.Sleep
    3. Access blocking collection directly and test the consumer

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -4,42 +4,63 @@ using MarketDataService;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Moq;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Collections.Concurrent;
 
 namespace UnitTestProject
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestProducerClient()
+        public readonly MarketDataUI.PricesUi p;
+        public readonly PrivateObject privateObject;
+
+        public UnitTest1()
         {
-            MarketDataUI.PricesUi p = new MarketDataUI.PricesUi();
-            var cancelTokenSrc = new CancellationTokenSource();
-            var token = cancelTokenSrc.Token;
-            var privateObject = new PrivateObject(p);
-            privateObject.Invoke("StartProducerTask", token);
-            privateObject.Invoke("StartConsumerTask", token);
-            Thread.Sleep(5000);
-            cancelTokenSrc.Cancel();
+            p = new MarketDataUI.PricesUi();
+            privateObject = new PrivateObject(p);
+        }
+        
 
-            privateObject.Invoke("StopTasks");
+        [TestMethod]
+        public void TestCoreDataProcessorWithMockClient()
+        {
+            var mock = new Mock<IPriceClient>();
+            mock.Setup(x => x.Start(0));           
+            mock.Object.OnPriceChanged += Client_OnPriceChanged;
 
-            IPriceClient client = new PriceClient();
-            var res = Task.Factory.StartNew(() => {
-                client.Start();
-            });
-            
-            client.OnPriceChanged += Client_OnPriceChanged;
-            Thread.Sleep(1000);
-            client.Stop();
-            res.Wait();
+            mock.Object.Start(0);
+            var priceChangedEvent = new PriceChangedEventArgs("A", 10, 20.3m, 21.3m, 11);
+            mock.Raise(item => item.OnPriceChanged += null, "sender", priceChangedEvent);
+
+            var list = (BindingList<Stock>)privateObject.GetField("_stocks");
+            //1. Check if the processed object in the bindinglist is the same as the event
+            Assert.AreEqual(list[0], ConvertEventObjectToStock(priceChangedEvent));
+
+            //2. Raise another event and check if the list contains the new object at index 1
+            var priceChangedEvent1 = new PriceChangedEventArgs("B", 11, 20.4m, 21.3m, 11);
+            mock.Raise(item => item.OnPriceChanged += null, "sender", priceChangedEvent1);
+
+            list = (BindingList<Stock>)privateObject.GetField("_stocks");
+            Assert.AreEqual(list[1], ConvertEventObjectToStock(priceChangedEvent1));
+
+            //3. Raise an event which updates the exisiting entry in the list @ index 0
+            var priceChangedEvent2 = new PriceChangedEventArgs("A", 211, 26.4m, 21.3m, 110);
+            mock.Raise(item => item.OnPriceChanged += null, "sender", priceChangedEvent2);
+
+            list = (BindingList<Stock>)privateObject.GetField("_stocks");
+            Assert.AreEqual(list[0], ConvertEventObjectToStock(priceChangedEvent2));
+
         }
 
-        private void Client_OnPriceChanged(object sender, PriceChangedEventArgs e)
+        public void Client_OnPriceChanged(object sender, PriceChangedEventArgs e)
         {
             //Check if Price client is raising events with valid data .
             //i.e. simple test check if symbol is always present
-            Assert.IsTrue(e.Symbol != "");           
+            Assert.IsTrue(e.Symbol != "");
+            privateObject.Invoke("ProcessData", ConvertEventObjectToStock(e));
         }
 
         [TestMethod]
@@ -55,13 +76,15 @@ namespace UnitTestProject
             var s2 = new MarketDataUI.Stock() { Symbol = "B", AskPrice = 10, BidPrice = 9, AskQty = 11, BidQty = 11 };
             Assert.AreNotEqual(s, s2);
             Assert.AreNotEqual(s.GetHashCode(), s2.GetHashCode());
+            Assert.AreEqual(s.GetHashCode(), s1.GetHashCode());
         }
 
         [TestMethod]
-        public void TestQueueWithConsumer()
+        public void TestQueueWithConsumerTaskRunning()
         {
-            MarketDataUI.PricesUi p = new MarketDataUI.PricesUi();
-            p.Queue.Add(new MarketDataUI.Stock()
+            var queue = (BlockingCollection<Stock>)privateObject.GetField("_queue");
+            
+            queue.Add(new MarketDataUI.Stock()
             {
                 Symbol = "A",
                 AskPrice = 10,
@@ -70,7 +93,7 @@ namespace UnitTestProject
                 AskQty = 12
             });
 
-            p.Queue.Add(new MarketDataUI.Stock()
+            queue.Add(new MarketDataUI.Stock()
             {
                 Symbol = "B",
                 AskPrice = 10,
@@ -79,15 +102,32 @@ namespace UnitTestProject
                 AskQty = 12
             });
 
-            var privateObject = new PrivateObject(p);
+            queue.CompleteAdding();
             var cancelTokenSrc = new CancellationTokenSource();
             var token = cancelTokenSrc.Token;
             privateObject.Invoke("StartConsumerTask", token);
 
-            //Introduce sleep and give consumer task a chance to clear the queue
-            //whilst it consumes data
-            Thread.Sleep(500);
-            Assert.AreEqual(p.Queue.Count, 0);           
+            while (!queue.IsCompleted)
+            {
+                //loop till queue is empty i.e. consumer has pulled out all elements
+            }
+
+            //Verify consumer has processed all elements in the queue
+            Assert.AreEqual(queue.Count, 0);
+        }
+
+        public static Stock ConvertEventObjectToStock(PriceChangedEventArgs e)
+        {
+            return new Stock()
+            {
+                Symbol = e.Symbol,
+                AskPrice = e.AskPrice,
+                BidPrice = e.BidPrice,
+                BidQty = e.BidQty,
+                AskQty = e.AskQty
+            };
         }
     }
+
+
 }
